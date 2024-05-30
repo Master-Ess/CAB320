e101d62f8b05c9da342a6fa185845ebffc703155
Lwhitt10 <luke.whitton@connect.qut.edu.au>
2024-05-30 19:33:37 +1000

    Update TransferLearning.py

--- Assignment_2/TransferLearning.py
+++ Assignment_2/TransferLearning.py
@@ -35,7 +35,7 @@ def load_model():
     Load in a model using the tf.keras.applications model and return it.
     MobileNetV2
     '''
-    model = keras.applications.MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+    model = tf.keras.applications.MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
     base_model = model
     base_model.trainable = False
 
